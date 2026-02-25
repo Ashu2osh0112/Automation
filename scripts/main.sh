ded286e8c550ffa8d2471840b910efced7608914
Sujal Singh <sujalsingh1920@gmail.com>
2026-02-25 18:17:12 +0530

    Apply conditions in main.sh to get single output of all 3 parameters in single file

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 source ../config/threshold.sh
+ 
+CPU_VALUE=$(./cpu_monitor.sh)
+DISK_VALUE=$(./disk_monitor.sh)
+MEMORY_VALUE=$(./memory_monitor.sh)
 
-./cpu_monitor.sh  
-./disk_monitor.sh  
-./memory_monitor.sh
+if [ "$CPU_VALUE" -gt "$CPU_LIMIT" ]; 
+then
+	echo "ERROR : $CPU_VALUE"
+else
+	echo " CPU IS OKAYY : '$CPU_VALUE'"
+fi
+
+if [ "$DISK_VALUE" -gt "$DISK_LIMIT" ];
+then
+        echo "ERROR : $CPU_VALUE"
+else
+        echo " DISK IS OKAYY : '$DISK_VALUE'"
+fi
+ 
+if [ "$MEMORY_VALUE" -gt "$RAM_LIMIT" ];
+then
+        echo "ERROR : $CPU_VALUE"
+else
+        echo " RAM IS OKAYY : '$MEMORY_VALUE'"
+fi
+
 
 
