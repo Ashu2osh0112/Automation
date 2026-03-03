ded9ba9267c9848dfcb00b6aa5c2a1e9ebdd4633
Sujal Singh <sujalsingh1920@gmail.com>
2026-03-03 12:17:54 +0530

    modified script file due to issue in absolute path to automated main script file and crontab have also modifed for path

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,52 +1,47 @@
-
 #!/bin/bash
 
-LOG_FILE="../logs/system.log"
+# -------------------- ENV FIX FOR CRON --------------------
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-source ../config/threshold.sh
+# -------------------- BASE PATH --------------------
+BASE_DIR="/nfs/Automation"
+LOG_FILE="$BASE_DIR/logs/system.log"
 
-CPU_VALUE=$(./cpu_monitor.sh)
-DISK_VALUE=$(./disk_monitor.sh)
-MEMORY_VALUE=$(./memory_monitor.sh)
+# -------------------- LOAD THRESHOLD --------------------
+source "$BASE_DIR/config/threshold.sh"
+
+# -------------------- GET VALUES --------------------
+CPU_VALUE=$("$BASE_DIR/scripts/cpu_monitor.sh")
+DISK_VALUE=$("$BASE_DIR/scripts/disk_monitor.sh")
+MEMORY_VALUE=$("$BASE_DIR/scripts/memory_monitor.sh")
 
 CURRENT_TIME=$(date '+%Y-%m-%d %H:%M:%S')
 
-if [ "$CPU_VALUE" -gt "$CPU_LIMIT" ];
-then
+# -------------------- STATUS CHECK --------------------
+if [ "$CPU_VALUE" -gt "$CPU_LIMIT" ]; then
     CPU_STATUS="ERROR"
 else
     CPU_STATUS="OK"
 fi
 
-
-if [ "$DISK_VALUE" -gt "$DISK_LIMIT" ];
-then
+if [ "$DISK_VALUE" -gt "$DISK_LIMIT" ]; then
     DISK_STATUS="ERROR"
 else
     DISK_STATUS="OK"
 fi
 
-
-if [ "$MEMORY_VALUE" -gt "$RAM_LIMIT" ]; 
-then
+if [ "$MEMORY_VALUE" -gt "$RAM_LIMIT" ]; then
     RAM_STATUS="ERROR"
 else
     RAM_STATUS="OK"
 fi
 
-
-# -------- Terminal Output (Single Line) --------
+# -------------------- TERMINAL OUTPUT --------------------
 echo "$CURRENT_TIME | CPU:$CPU_VALUE ($CPU_STATUS) | DISK:$DISK_VALUE ($DISK_STATUS) | RAM:$MEMORY_VALUE ($RAM_STATUS)"
 
-
-# -------- Append Data Row --------
-{
+# -------------------- LOG FILE ENTRY --------------------
 printf "%-20s | %-20s | %-20s | %-20s\n" \
 "$CPU_VALUE ($CPU_STATUS)" \
 "$DISK_VALUE ($DISK_STATUS)" \
 "$MEMORY_VALUE ($RAM_STATUS)" \
 "$CURRENT_TIME" >> "$LOG_FILE"
-
-} | tee -a "$LOG_FILE"
-
-
